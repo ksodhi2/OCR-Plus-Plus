59fccd1b6e272c05d31b287c2a4e946c514ddb33
ksodhi2 <ksodhi2@illinois.edu>
2020-04-20 13:42:17 -0400

    Built OpenCV from source

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,8 +1,8 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
+#include <opencv2/opencv.hpp>
 #include "my_app.h"
 #include <cinder/app/App.h>
-
 
 namespace myapp {
 
@@ -10,7 +10,22 @@ using cinder::app::KeyEvent;
 
 MyApp::MyApp() { }
 
-void MyApp::setup() { }
+void MyApp::setup() {
+  cv::VideoCapture cap(0);
+
+  cv::Mat edges;
+  cv::namedWindow("edges",1);
+  for(;;)
+  {
+    cv::Mat frame;
+    cap >> frame; // get a new frame from camera
+    cv::cvtColor(frame, edges, cv::COLOR_BGR2GRAY);
+    cv::GaussianBlur(edges, edges, cv::Size(7,7), 1.5, 1.5);
+    cv::Canny(edges, edges, 0, 30, 3);
+    imshow("edges", edges);
+    if(cv::waitKey(30) >= 0) break;
+  }
+}
 
 void MyApp::update() { }
 
