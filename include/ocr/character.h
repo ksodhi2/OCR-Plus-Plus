344a30b97ea80788ddffbee8957cbadba3be808f
ksodhi2 <ksodhi2@illinois.edu>
2020-04-28 17:06:44 -0400

    Refactored Character class

---
+++ include/ocr/character.h
@@ -0,0 +1,59 @@
+// Copyright 2020 [Karan Sodhi]. All rights reserved.
+
+#ifndef FINALPROJECT_CHARACTER_H
+#define FINALPROJECT_CHARACTER_H
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+
+namespace ocr {
+  
+const size_t kCharacterSize = 28;
+
+class Character {
+ private:
+  /**
+   * A 28x28 2D vector containing all the pixels of the Character
+   * 0 is the most white value while 255 is the most black value
+   */
+  std::vector<std::vector<size_t>> pixels_;
+  
+ public:
+  /**
+   * Fills the pixel array with the pixel value from the image
+   * @param character_image a 28x28 black and white image of a character
+   */
+  Character(const cv::Mat& character_image);
+  Character();
+  
+  /**
+   * Fills the pixel array with the imputed value
+   * @param value the value of the pixel to input (0 - 255)
+   * @param row the row where the pixel is located
+   * @param col the column where the pixel is located
+   */
+  void FillPixel(size_t value, size_t row, size_t col);
+
+  /**
+   * @param row the row where the pixel is located
+   * @param col the column where the pixel is located
+   * @return the pixel value at the specified location
+   */
+  int PixelValueAt(size_t row, size_t col) const;
+
+  /**
+   * Prints out a representation of the character to a generic output stream
+   * '#" means that the pixel at that location is more black
+   * while whitespace means the pixel is more white
+   * @param os the output stream
+   * @param character the character to print out
+   * @return the output stream
+   */
+  friend std::ostream& operator<<(std::ostream& os, const Character& character);
+};
+
+}  // namespace ocr
+
+
+#endif  // FINALPROJECT_CHARACTER_H
--- src/character.cc
+++ src/character.cc
@@ -1,44 +1,47 @@
 // Copyright (c) 2020 [Karan Sodhi]. All rights reserved.
 
-#include <mylibrary/character.h>
-#include <iostream>
+#include <ocr/character.h>
 
-namespace mylibrary {
+namespace ocr {
 
-Character::Character() {
-  pixels_.resize(kCharacterSize, std::vector<int>(kCharacterSize));
-}
+Character::Character(): pixels_(kCharacterSize, std::vector<size_t>(kCharacterSize)) {}
 
-/**
- * Fills the pixel array with the pixel value from the image
- */
-Character::Character(cv::Mat character_image) {
-  pixels_.resize(kCharacterSize, std::vector<int>(kCharacterSize));
-  for (int row = 0; row < character_image.rows; row++) {
-    for (int col = 0; col < character_image.cols; col++) {
-      int pixel_value = static_cast<int>(character_image.at<uchar>(row, col));
+Character::Character(const cv::Mat& character_image) : Character() {
+  if (character_image.rows != kCharacterSize 
+      || character_image.cols != kCharacterSize) {
+    throw std::invalid_argument("Must pass in 28x28 image");
+  }
+  
+  for (size_t row = 0; row < character_image.rows; row++) {
+    for (size_t col = 0; col < character_image.cols; col++) {
+      size_t pixel_value = static_cast<int>(character_image.at<uchar>(row, col));
       pixels_[row][col] = pixel_value;
     }
   }
 }
 
-void Character::PrintPixels() const {
-  for (int row = 0; row < kCharacterSize; row++) {
-    for (int col = 0; col < kCharacterSize; col++) {
-      int pixel_value = pixels_[row][col];
-      if (pixel_value >= 128) {
-        std::cout << '#';
-      } else {
-        std::cout << ' ';
-      }
-    }
-    std::cout << std::endl;
-  }
-}
-
-void Character::FillPixel(int value, int row, int col) {
+void Character::FillPixel(size_t value, size_t row, size_t col) {
   pixels_[row][col] = value;
 }
 
+int Character::PixelValueAt(size_t row, size_t col) const { 
+  return pixels_[row][col]; 
 }
+
+std::ostream& operator<<(std::ostream& os, const Character& character) {
+  for (size_t row = 0; row < kCharacterSize; row++) {
+    for (size_t col = 0; col < kCharacterSize; col++) {
+      int pixel_value = character.pixels_[row][col];
+      if (pixel_value >= 128) {
+        os.put('#');
+      } else {
+        os.put(' ');
+      }
+    }
+    os.put('\n');
+  }
+  return os;
+}
+
+}  // namespace ocr
 
