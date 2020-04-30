50d075ed9db92b3f3462ae1ef8e71c35e10a4d1a
ksodhi2 <ksodhi2@illinois.edu>
2020-04-29 21:53:29 -0400

    Added test for image transcriber class

--- tests/image_transcriber_test.cc
+++ tests/image_transcriber_test.cc
@@ -12,9 +12,11 @@ TEST_CASE("Read Image From File") {
   SECTION("Try to Read image from file that does not exist") {
     REQUIRE_THROWS(image_transcriber.ReadImageFromFile("does/not/exist"));
   }
+  
   SECTION("Try to Read Image from a non image file") {
     REQUIRE_THROWS(image_transcriber.ReadImageFromFile("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/emnist_test.csv"));
   }
+  
   SECTION("Try to Read a valid image file") {
     REQUIRE_NOTHROW(image_transcriber.ReadImageFromFile("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/assets/letters.png"));
   }
