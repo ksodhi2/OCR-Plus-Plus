02560acd4f05ef85a7debe332eef634b412e0541
ksodhi2 <ksodhi2@illinois.edu>
2020-04-29 21:50:13 -0400

    Added Tests for Training Data class

---
+++ tests/training_data_test.cc
@@ -0,0 +1,82 @@
+// Copyright (c) 2020 [Karan Sodhi]. All rights reserved.
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include <ocr/training_data.h>
+
+using ocr::TrainingData;
+
+ocr::TrainingData training_data("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/emnist_test.csv");
+
+TEST_CASE("Constructing training data from a file") {
+  SECTION("Trying to construct training data from a file that does not exist") {
+    REQUIRE_THROWS(TrainingData("does/not/exist"));
+  }
+  SECTION("Trying to construct training data from an empty file") {
+    REQUIRE_THROWS(TrainingData("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/empty.csv"));
+  }
+  SECTION("Trying to construct training data from a file with non enough values") {
+    REQUIRE_THROWS(TrainingData("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/not_enough.csv"));
+  }
+  SECTION("Trying to construct training data from a file with non integer values") {
+    REQUIRE_THROWS(TrainingData("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/non_integer.csv"));
+  }
+  SECTION("Trying to construct training data from a file with too big and small values") {
+    REQUIRE_THROWS(TrainingData("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/too_big_and_small.csv"));
+  }
+  SECTION("Trying to construct training data from a valid file") {
+    REQUIRE_NOTHROW(TrainingData("/Users/karan/Documents/cinder_0.9.2_mac/my-projects/final-project-ksodhi2/tests/data/emnist_test.csv"));
+  }
+}
+
+TEST_CASE("Size of training Data") {
+  REQUIRE(training_data.Size() == 10000);
+}
+
+TEST_CASE("Get Training Samples") {
+  ocr::LabeledCharacter labeled_character = training_data.GetTrainingSampleAt(500);
+  SECTION("Correct label for Training sample") {
+    REQUIRE(labeled_character.label == '0');
+  }
+  SECTION("Correct character for Training sample") {
+    
+    SECTION("Correct Pixel Values") {
+      REQUIRE(labeled_character.character.PixelValueAt(5, 17) == 254);
+    }
+    
+    SECTION("Correct Printed Representation") {
+      std::ostringstream printed_character;
+      printed_character << labeled_character.character;
+      string printed_text = string("                            ") +
+                     '\n' + string("                            ") +
+                     '\n' + string("                            ") +
+                     '\n' + string("           #######          ") +
+                     '\n' + string("          ##########        ") +
+                     '\n' + string("        #############       ") +
+                     '\n' + string("       ###############      ") +
+                     '\n' + string("      ####       ######     ") +
+                     '\n' + string("      ###          ####     ") +
+                     '\n' + string("     ####          #####    ") +
+                     '\n' + string("    ####            ####    ") +
+                     '\n' + string("    ###              ###    ") +
+                     '\n' + string("    ###              ####   ") +
+                     '\n' + string("    ###              ####   ") +
+                     '\n' + string("   ####               ###   ") +
+                     '\n' + string("   ###                ###   ") +
+                     '\n' + string("   ###                ###   ") +
+                     '\n' + string("   ####               ###   ") +
+                     '\n' + string("   ####               ###   ") +
+                     '\n' + string("    ###              ###    ") +
+                     '\n' + string("    ####             ###    ") +
+                     '\n' + string("     ####         ####      ") +
+                     '\n' + string("      ###############       ") +
+                     '\n' + string("       ############         ") +
+                     '\n' + string("         #######            ") +
+                     '\n' + string("                            ") +
+                     '\n' + string("                            ") +
+                     '\n' + string("                            ") + '\n';
+      REQUIRE(printed_character.str() == printed_text);
+    }
+  }
+}
