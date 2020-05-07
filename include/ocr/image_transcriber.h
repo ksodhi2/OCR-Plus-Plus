c63210b36a311298ee55689d70a818ddddd1ecfe
ksodhi2 <ksodhi2@illinois.edu>
2020-05-06 21:58:49 -0400

    Refactored Image Transcriber class

--- include/ocr/image_transcriber.h
+++ include/ocr/image_transcriber.h
@@ -1,12 +1,13 @@
 // Copyright (c) 2020 [Karan Sodhi]. All rights reserved.
 
-#ifndef FINALPROJECT_MYLIBRARY_EXAMPLE_H_
-#define FINALPROJECT_MYLIBRARY_EXAMPLE_H_
+#ifndef INCLUDE_OCR_IMAGE_TRANSCRIBER_H_
+#define INCLUDE_OCR_IMAGE_TRANSCRIBER_H_
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/text.hpp>
 #include <ocr/knn.h>
 #include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/text.hpp>
 
 using cv::Mat;
 using std::vector;
@@ -18,22 +19,24 @@ namespace ocr {
 * more info here https://github.com/MhLiao/TextBoxes
 * https://docs.opencv.org/master/d1/d66/classcv_1_1text_1_1TextDetectorCNN.html
 */
-const string text_detection_model = "data/TextBoxes_icdar13.caffemodel";
-const string text_detection_description = "data/textbox.prototxt";
+const string kTextDetectionModel = "data/TextBoxes_icdar13.caffemodel";
+const string kTestDetectionDescription = "data/textbox.prototxt";
+
+const int kMinCharacterArea = 10000;
+const double kMinTextConfidence = .6;
 
 class ImageTranscriber {
-  
  private:
   const Classifier& classifier;
-  
+
   /**
    * Finds text from an image and attempts to transcribe it 
    * by displaying the correct characters above the text
-   * @param image the image to read
-   * @return a transcribed image
+   * @param input_image the image to read
+   * @return a transcribed image with text overlaid on top
    */
-  Mat ReadImage(const Mat& image);
-  
+  Mat OverlayTextOnImage(const Mat& input_image);
+
   /**
    * Processes the image by greying it out, smoothing it, and thresholding it
    * @param input_image the image that was imputed
@@ -53,28 +56,27 @@ class ImageTranscriber {
    * @param rect_list a list of rectangles
    * @return if the single rectangle overlaps with any of the rectangles in the list
    */
-  bool DoesRectangleOverlap(const cv::Rect& rectangle, const vector<cv::Rect>& rect_list);
-  
+  bool DoRectanglesOverlap(const cv::Rect& rectangle,
+                           const vector<cv::Rect>& rect_list);
+
  public:
-  ImageTranscriber(const Classifier& classifier);
-  
+  explicit ImageTranscriber(const Classifier& classifier);
+
   /**
    * Uses an image file to find text from an image and transcribe it
    * if the file path is not valid throws an exception
    * @param file_path the file path of the image to transcribe
    * @return a transcribed image
    */
-  Mat ReadImageFromFile(const std::string& file_path);
+  Mat TranscribeImageFromFile(const std::string& file_path);
 
   /**
    * Finds text from a web cam and attempts to transcribe it live
    * @return the transcribed image frame from the web cam
    */
-  Mat ReadImageFromCamera();
-  
+  Mat TranscribeImageFromCamera();
 };
 
 }  // namespace ocr
 
-
-#endif // FINALPROJECT_MYLIBRARY_EXAMPLE_H_
+#endif  // INCLUDE_OCR_IMAGE_TRANSCRIBER_H_
--- src/image_transcriber.cc
+++ src/image_transcriber.cc
@@ -4,29 +4,35 @@
 
 namespace ocr {
 
-ImageTranscriber::ImageTranscriber(const Classifier& classifier) : classifier(classifier) {}
+ImageTranscriber::ImageTranscriber(const Classifier& classifier)
+    : classifier(classifier) {}
 
-Mat ImageTranscriber::ReadImage(const Mat& input_image) {
+Mat ImageTranscriber::OverlayTextOnImage(const Mat& input_image) {
   Mat processed_image = ProcessImage(input_image);
-  
+
   vector<vector<cv::Point>> contours;
-  cv::findContours(processed_image, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+  cv::findContours(processed_image, contours,
+                   cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
   vector<cv::Rect> text_areas = FindAreaWithText(processed_image);
-  
+
   for (const auto& contour : contours) {
     cv::Rect boundingRect = cv::boundingRect(contour);
-    if (boundingRect.area() < 5000 || !DoesRectangleOverlap(boundingRect, text_areas)) {
+    // checking if contour corresponds to an actual character
+    if (boundingRect.area() < kMinCharacterArea ||
+        !DoRectanglesOverlap(boundingRect, text_areas)) {
       continue;
     }
+
     Mat character_region = processed_image(boundingRect);
-    cv::dilate(character_region, character_region, cv::Mat(9, 9, 0));
-    cv::resize(character_region, character_region, cv::Size(20, 20), cv::INTER_AREA);
-    cv::copyMakeBorder(character_region, character_region,4, 4, 4, 4, cv::BORDER_CONSTANT);
     ocr::Character character(character_region);
-    
-    cv::putText(input_image, string(1, classifier.Classify(character)), 
-                cv::Point(boundingRect.x, boundingRect.y),
-                cv::FONT_HERSHEY_SIMPLEX, 5, cv::Scalar(0, 255, 0), 3);
+
+    string predicted_character = string(1, classifier.Classify(character));
+    cv::Point text_location(boundingRect.x, boundingRect.y);  // upper right corner
+    int text_size = 5;
+    int text_thickness = 3;
+    cv::Scalar text_color(0, 255, 0);  // green
+    cv::putText(input_image, predicted_character, text_location,
+                cv::FONT_HERSHEY_SIMPLEX, text_size, text_color, text_thickness);
   }
   return input_image;
 }
@@ -35,23 +41,23 @@ vector<cv::Rect> ImageTranscriber::FindAreaWithText(const Mat& input_image) {
   Mat image;
   cv::cvtColor(input_image, image, cv::COLOR_GRAY2BGR);
   vector<cv::Rect> rects;
-  vector<cv::Rect> viable_rects;
+  vector<cv::Rect> rects_with_text;
   vector<float> confidence;
   cv::text::TextDetectorCNN::create
-      (text_detection_description, text_detection_model)
+      (kTestDetectionDescription, kTextDetectionModel)
           ->detect(image, rects, confidence);
-  
+
   for (int i = 0; i < rects.size(); i++) {
-    if (confidence[i] > .5) {
-      viable_rects.push_back(rects[i]);
+    if (confidence[i] > kMinTextConfidence) {
+      rects_with_text.push_back(rects[i]);
     }
   }
-  return rects;
+  return rects_with_text;
 }
 
-bool ImageTranscriber::DoesRectangleOverlap(const cv::Rect& rectangle, 
-                                            const vector<cv::Rect>& rect_list) {
-  for (const cv::Rect& rect: rect_list) {
+bool ImageTranscriber::DoRectanglesOverlap(const cv::Rect& rectangle,
+                                           const vector<cv::Rect>& rect_list) {
+  for (const cv::Rect& rect : rect_list) {
     if ((rectangle & rect).area() > 0) {
       return true;
     }
@@ -63,23 +69,24 @@ Mat ImageTranscriber::ProcessImage(const Mat& input_image) {
   Mat processed_image;
   cv::cvtColor(input_image, processed_image, cv::COLOR_BGR2GRAY);
   cv::GaussianBlur(processed_image, processed_image, cv::Size(5, 5), 0);
-  cv::threshold(processed_image, processed_image, 0, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
+  cv::threshold(processed_image, processed_image, kMinPixelValue, kMaxPixelValue,
+                cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
   return processed_image;
 }
 
-Mat ImageTranscriber::ReadImageFromFile(const std::string& file_path) {
+Mat ImageTranscriber::TranscribeImageFromFile(const std::string& file_path) {
   Mat input = cv::imread(file_path);
   if (input.empty()) {
     throw std::invalid_argument("Must pass in valid image file");
   }
-  return ReadImage(input);
+  return OverlayTextOnImage(input);
 }
 
-Mat ImageTranscriber::ReadImageFromCamera() {
-  cv::VideoCapture cap(0);
+Mat ImageTranscriber::TranscribeImageFromCamera() {
+  cv::VideoCapture capture(0);
   Mat input;
-  cap >> input;
-  return ReadImage(input);
+  capture >> input;
+  return OverlayTextOnImage(input);
 }
 
 }  // namespace ocr
